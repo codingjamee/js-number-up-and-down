bbc5d296144bec386198a162498425f605fd3a06
허제인 <codingjamee@gmail.com>
2024-05-26 00:04:40 +0900

    random number만들기함수 및 변수 선언

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import readline from "readline";
 
 function readLineAsync(query) {
+  function createRandomNumber() {
+    return Math.floor(Math.random() * 50 + 1);
+  }
+  const answer = createRandomNumber();
+  let count = 5;
+  console.log(answer);
+
+  function printUpDown(input) {
+    if (answer > input) {
+      return console.log("up");
+    }
+    if (answer < input) {
+      return console.log("down");
+    }
+  }
+
+  function subtractCount() {
+    count--;
+  }
+
   return new Promise((resolve, reject) => {
     if (arguments.length !== 1) {
       reject(new Error("arguments must be 1"));
@@ -16,16 +36,22 @@ function readLineAsync(query) {
     });
 
     rl.question(query, (input) => {
-      rl.close();
-      resolve(input);
+      const inputNum = +input;
+      if (+inputNum === answer) {
+        rl.close();
+        return resolve(inputNum);
+      }
+
+      printUpDown(inputNum);
+      subtractCount();
     });
   });
 }
 
-// 입출력 예시
 async function play() {
   console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
   const inputValue = await readLineAsync("숫자 입력: ");
+  console.log("정답!");
   console.log(inputValue);
 }
 
