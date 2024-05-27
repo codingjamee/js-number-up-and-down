eea6b555b7b9b6510fa537c51ebd668a11318525
허제인 <codingjamee@gmail.com>
2024-05-27 21:01:41 +0900

    요구사항 수정 prevGuess 즉시실행함수 설정 재시작시 guess array 초기화 printResult 함수 설정

--- src/index.js
+++ src/index.js
@@ -26,40 +26,65 @@ function createRandomNumber() {
   return Math.floor(Math.random() * 50 + 1);
 }
 
+const prevGuess = (() => {
+  let guess = [];
+  const guessInputFn = (guessInput) => {
+    guess.push(+guessInput);
+    console.log(guessInput);
+    console.log("이전 추측:", guess.join(", "));
+  };
+  const resetGuess = () => (guess = []);
+  return { guessInputFn, resetGuess };
+})();
+
 function printUpDown(answer, input) {
   if (answer > input) {
-    console.log("up");
+    console.log("업");
   }
   if (answer < input) {
-    console.log("down");
+    console.log("다운");
   }
 }
 
-async function restart() {
+async function askRestart() {
   const restartOrNot = await readLineAsync(
-    "재시작 하시겠습니까?(yes or no) : "
+    "게임을 다시 시작하시겠습니까? (yes/no): "
   );
   if (restartOrNot === "yes") {
-    play();
+    prevGuess.resetGuess();
+    return play();
   }
+  console.log("게임을 종료합니다.");
+}
+
+function printResult(result, count, answer) {
+  if (result === "success") {
+    console.log("정답!");
+    console.log(`축하합니다! ${count}번 만에 숫자를 맞추셨습니다.`);
+    return askRestart();
+  }
+  if (result === "fail")
+    console.log(`5회 초과! 숫자를 맞추지 못했습니다. (정답: ${answer})`);
+  return askRestart();
 }
 
 async function play() {
   const answer = createRandomNumber();
   console.log(answer);
   let cnt = 1;
+  console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
   while (cnt <= 5) {
-    console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
     const inputValue = await readLineAsync("숫자 입력: ");
     if (+inputValue === answer) {
-      console.log("정답!");
-      console.log(cnt + " 만에 성공");
+      printResult("success", cnt);
       break;
     }
+    prevGuess.guessInputFn(inputValue);
     printUpDown(answer, inputValue);
     cnt++;
   }
-  restart();
+  //5회 초과시
+  if (cnt > 5) printResult("fail", cnt, answer);
 }
 
 play();
