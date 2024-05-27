d651d094f8289b1df9da80fcad2888fe66f5f1d0
허제인 <codingjamee@gmail.com>
2024-05-27 21:47:41 +0900

    Refactor: 즉시실행함수로 클로저 반환

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import readline from "readline";
 
+const init = (function () {
+  const createRandomNumber = () => {
+    return Math.floor(Math.random() * 50 + 1);
+  };
+  const answer = createRandomNumber();
+  console.log(answer);
+  let cnt = 1;
+  const getCnt = () => cnt;
+  const addCnt = () => cnt++;
+  const resetCnt = () => (cnt = 1);
+  const resetAnswer = () => createRandomNumber();
+  return { getCnt, addCnt, resetCnt, answer, resetAnswer };
+})();
+
+const initGuess = (function () {
+  let guess = [];
+  const showInput = (guessInput) => {
+    guess.push(+guessInput);
+    console.log(guessInput);
+    console.log("이전 추측:", guess.join(", "));
+  };
+  const resetGuess = () => (guess = []);
+  return { showInput, resetGuess };
+})();
+
+const { getCnt, addCnt, resetCnt, answer, resetAnswer } = init;
+const {showInput, resetGuess} = initGuess;
+
 function readLineAsync(query) {
   return new Promise((resolve, reject) => {
     if (arguments.length !== 1) {
@@ -22,22 +50,7 @@ function readLineAsync(query) {
   });
 }
 
-function createRandomNumber() {
-  return Math.floor(Math.random() * 50 + 1);
-}
-
-const prevGuess = (() => {
-  let guess = [];
-  const guessInputFn = (guessInput) => {
-    guess.push(+guessInput);
-    console.log(guessInput);
-    console.log("이전 추측:", guess.join(", "));
-  };
-  const resetGuess = () => (guess = []);
-  return { guessInputFn, resetGuess };
-})();
-
-function printUpDown(answer, input) {
+function printUpDown(input) {
   if (answer > input) {
     console.log("업");
   }
@@ -51,16 +64,19 @@ async function askRestart() {
     "게임을 다시 시작하시겠습니까? (yes/no): "
   );
   if (restartOrNot === "yes") {
-    prevGuess.resetGuess();
+    resetAnswer();
+    resetCnt();
+    resetGuess();
     return play();
   }
   console.log("게임을 종료합니다.");
 }
 
-function printResult(result, count, answer) {
+
+function printResult(result) {
   if (result === "success") {
     console.log("정답!");
-    console.log(`축하합니다! ${count}번 만에 숫자를 맞추셨습니다.`);
+    console.log(`축하합니다! ${getCnt()}번 만에 숫자를 맞추셨습니다.`);
     return askRestart();
   }
   if (result === "fail")
@@ -69,24 +85,20 @@ function printResult(result, count, answer) {
 }
 
 async function play() {
-  const answer = createRandomNumber();
-  console.log(answer);
-  let cnt = 1;
   console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
-  while (cnt <= 5) {
+  while (getCnt() <= 5) {
     const inputValue = await readLineAsync("숫자 입력: ");
     if (+inputValue === answer) {
-      printResult("success", cnt);
+      printResult("success");
       break;
     }
-    if (cnt !== 5) {
-      prevGuess.guessInputFn(inputValue);
-      printUpDown(answer, inputValue);
+    if (getCnt() < 5) {
+      showInput(inputValue);
+      printUpDown(inputValue);
     }
-    cnt++;
+    addCnt();
   }
-  //5회 초과시
-  if (cnt > 5) printResult("fail", cnt, answer);
+  if (getCnt() > 5) printResult("fail");
 }
 
 play();
