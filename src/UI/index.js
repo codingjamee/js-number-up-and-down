40449eb76af3b93f523bc9ff58ed380f290dee12
허제인 <codingjamee@gmail.com>
2024-06-03 00:22:06 +0900

    재사용성을 위해 startSetting을 함수로 분리 tryLoop도 같은 위계를 맞추기 위해 분리 자주 쓰이는 validtaion 숫자임을 확인하기 위한 함수 분리

--- src/UI/index.js
+++ src/UI/index.js
@@ -1,12 +1,18 @@
-import readline from "readline";
-import { gameData } from "../domain/index.js";
-
 /**
 계산 로직과 다른점은
 계산은 사용자와의 인터렉션에 관련이 없으나
 UI는 사용자와의 인터렉션에 관련이 있음
 그러므로 count에 따라 보여줘야할지 말지는 UI에서 다뤄야 한다고 판단했음
  */
+import readline from "readline";
+import { gameData } from "../domain/index.js";
+
+const constant = {
+  askBoundary: "게임 시작을 위해 최소 값, 최대 값을 입력해주세요. (예: 1, 50)",
+  askTrialLimit: "게임 시작을 위해 진행 가능 횟수를 입력해주세요.",
+  askRestart: "게임을 다시 시작하시겠습니까? (yes/no): ",
+};
+
 /**
 사용자 입력을 받는다
 최소 최대값 입력을 받는다 
@@ -15,49 +21,66 @@ UI는 사용자와의 인터렉션에 관련이 있음
 */
 
 export async function playGame() {
-  const boundary = await askBoundary();
-  const [minNumber, maxNumber] = boundary.split(",").trim();
-  const trialLimit = await askCount();
-  let trial = 1; //trial 재할당을 위해 let으로 선언
+  let { answer, trialLimit } = await startSetting();
+  console.log({ answer, trialLimit });
+  let trial = 1;
+  trial = await tryLoop(trial, trialLimit, answer);
+}
+
+async function startSetting() {
+  const boundary = await readLineAsync(constant.askBoundary);
+  const [minNumber, maxNumber] = boundary.split(",");
+  //input값 validation 설정
+  const validateBoundary = validation(minNumber, maxNumber);
+  if (!validateBoundary) return askRestart();
+
+  const trialLimit = await readLineAsync(constant.askTrialLimit)
+  const validateLimit = validation(trialLimit);
+  if (!validateLimit) return askRestart();
+
   const answer = gameData(minNumber, maxNumber).answer;
   console.log(
     `컴퓨터가 ${minNumber}~${maxNumber} 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.`
   );
-  trial = await tryLoop(trial, trialLimit, answer);
+  return { answer, trialLimit };
+}
 
-  askRestart();
+function validation(...arg) {
+  for (let i = 0; i < arg.length; i++) {
+    if (isNaN(arg[i])) {
+      console.log("유효하지 않은 숫자입니다. 다시 입력해주세요.");
+      return false;
+    }
+  }
+  return true;
 }
 
 async function tryLoop(trial, trialLimit, answer) {
   let trialCount = trial;
+  const guess = [];
   while (trial <= trialLimit) {
     const inputValue = await readLineAsync("숫자 입력: ");
-    const typeIsNumber = !isNaN(inputValue);
-    if (!typeIsNumber) {
+    const inputValid = validation(inputValue);
+    if (!inputValid) {
       console.log("숫자가 아닙니다.");
       continue;
     }
     if (parseFloat(inputValue) === answer) {
-      printResult("success", trial);
+      printResult({ result: "success", trial });
+      askRestart(); //여기다 둬도 동작 할지 확인
       break;
     }
-    if (trialCount < 5) {
-      showInput(inputValue);
+    if (trialCount <= trialLimit) {
+      guess.push(gameData().showInput(guess, inputValue));
       gameData().printUpDown(answer, inputValue);
     }
-    if (trialCount > trialLimit) return printResult("fail");
-    if (typeIsNumber) trialCount--;
+    if (trialCount > trialLimit) {
+      printResult({ result: "fail", answer: answer });
+      return askRestart();
+    }
+    if (typeIsNumber) trialCount++;
   }
   return trialCount;
-}
-
-async function askBoundary() {
-  return await readLineAsync(
-    "게임 시작을 위해 최소 값, 최대 값을 입력해주세요. (예: 1, 50)"
-  );
-}
-async function askCount() {
-  return await readLineAsync("게임 시작을 위해 진행 가능 횟수를 입력해주세요.");
 }
 
 function readLineAsync(query) {
@@ -83,27 +106,22 @@ function readLineAsync(query) {
 }
 
 async function askRestart() {
-  const restartOrNot = await readLineAsync(
-    "게임을 다시 시작하시겠습니까? (yes/no): "
-  );
+  const restartOrNot = await readLineAsync(constant.askRestart);
   if (restartOrNot === "yes") {
-    resetAnswer();
-    resetCount();
-    resetGuess();
-    return play();
+    return playGame();
   }
   console.log("게임을 종료합니다.");
 }
 
-function printResult(result, trial) {
+function printResult({ result, trial, answer }) {
+  //외부 의존성을 줄일 수는 없을까?
+  //여기서 외부 의존성이라는건 매개변수
   if (result === "success") {
     console.log("정답!");
     console.log(`축하합니다! ${trial}번 만에 숫자를 맞추셨습니다.`);
-    return askRestart();
+    return;
   }
   if (result === "fail")
-    console.log(
-      `5회 초과! 숫자를 맞추지 못했습니다. (정답: ${gameData().answer})`
-    );
-  return askRestart();
+    console.log(`5회 초과! 숫자를 맞추지 못했습니다. (정답: ${answer})`);
+  return;
 }
