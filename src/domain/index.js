788dcaf53cd80a6b032c20478c746d655775eb41
허제인 <codingjamee@gmail.com>
2024-06-03 00:26:57 +0900

    showInput로직 domain에 추가

--- src/domain/index.js
+++ src/domain/index.js
@@ -1,9 +1,9 @@
 /**
  * domain으로 로직을 분리하려면
 굳이 한 파일에서 전역변수를 피할 필요가 없으므로 
-즉시실행함수는 사용하지 않아도 됨
+즉시실행함수는 사용하지 않아도 됨 
 domain의 하위의 모듈은 view하위의 모듈에 의존하지 말아야 한다는 의미는
-view에서 호출한 인수를 변경하면 안된다는 것으로 해석?
+view에서 전달한 인수를 변경하면 안된다는 것으로 해석?
  */
 /**
 계산하고 결과를 출력하는 로직
@@ -14,11 +14,11 @@ view에서 호출한 인수를 변경하면 안된다는 것으로 해석?
  */
 
 export function gameData(min, max) {
-  //game 객체를 활용한 이유?
-  //하나의 객체로 UI 로직을 호출하여 데이터를 보내주기 위해
+  //객체를 활용한 이유 하나의 객체로 UI 로직을 호출하여 데이터를 보내주기 위해
   return {
     answer: createRandomNumber(min, max),
     printUpDown,
+    showInput,
   };
 
   function createRandomNumber(min, max) {
@@ -26,6 +26,7 @@ export function gameData(min, max) {
     const maxNumber = max;
     return Math.floor(Math.random() * maxNumber + minNumber);
   }
+
   function printUpDown(answer, input) {
     if (answer > input) {
       console.log("업");
@@ -34,11 +35,13 @@ export function gameData(min, max) {
       console.log("다운");
     }
   }
-  function showInput(guessInput) {
-    const guess = [];
+
+  function showInput(guessArr, guessInput) {
+    const guess = guessArr;
     const toNumberInput = parseFloat(guessInput);
-    guess.push(toNumberInput);
+    if(typeof guess === 'array') guess.push(toNumberInput);
     console.log(toNumberInput);
     console.log("이전 추측:", guess.join(", "));
+    return guess;
   }
 }
