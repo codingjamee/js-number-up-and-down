8a2acebbec01876322c7d872988f9095f172c7f5
허제인 <codingjamee@gmail.com>
2024-06-12 21:31:23 +0900

    Feat: 초기 세팅 기능구현

--- src/UI/index.js
+++ src/UI/index.js
@@ -1,79 +1,96 @@
-/**
-계산 로직과 다른점은
-계산은 사용자와의 인터렉션에 관련이 없으나
-UI는 사용자와의 인터렉션에 관련이 있음
-그러므로 count에 따라 보여줘야할지 말지는 UI에서 다뤄야 한다고 판단했음
- */
 import readline from "readline";
-import { gameData, InitialGameConfig } from "../domain/index.js";
-import { gameStatus } from "../domain/index.js";
+import {
+  gameStatus,
+  InitialGameConfig,
+  checkPromptNumber,
+  createRandomNumber,
+  settingConfig,
+  changeStatus,
+  returnSettingQuestion,
+  returnSettingErrorMessage,
+} from "../domain/index.js";
 
-/**
- * 따로 constant 객체로 뺀 이유
- * 나중에 문구를 수정할 때 묶어두면 편함
- * 프로퍼티 명으로 문구 빠르게 유추 가능
- */
 
-const constant = {
-  askBoundary: "게임 시작을 위해 최소 값, 최대 값을 입력해주세요. (예: 1, 50) ",
-  askTrialLimit: "게임 시작을 위해 진행 가능 횟수를 입력해주세요.",
-  askRestart: "게임을 다시 시작하시겠습니까? (yes/no): ",
-  restart: "게임을 다시 시작합니다.",
-};
-
-const getGameStatusMessages = ({
-  notValidType,
-  answer,
-  trialCount,
-  minNumber,
-  maxNumber,
-}) => {
-  return {
-    notValid: `입력하신 ${notValidType}이 유효하지 않습니다.`,
-    guessNumber: `컴퓨터가 ${minNumber}~${maxNumber} 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.`,
-    correct: `정답! 
-  축하합니다! ${trialCount}번 만에 숫자를 맞추셨습니다.`,
-    fail: `5회 초과! 숫자를 맞추지 못했습니다. (정답: ${answer})`,
-    end: "게임을 종료합니다.",
-  };
-};
-
-/**
-사용자 입력을 받는다
-최소 최대값 입력을 받는다 
-재시작 종료 옵션
-몇회만에 맞췄는지 보여준다
-*/
 
 export async function startGame() {
+  const { promptCount, totalSettingCount } = { ...settingConfig };
   const state = initializeGameSetting();
 
-  while (state.status === gameStatus.USERSETTING) {
-    promptUserSetting();
-  }
+  const whileCondition = promptCount <= totalSettingCount;
+  loopWhileCondition(promptUserSetting, whileCondition);
+
+  const targetStatus = gameStatus.USERSETTING_COUNT[promptCount];
+  changeStatus(state.status, targetStatus);
+  computerSetting();
 
   return playGame(state);
 
+  function computerSetting(state) {
+    const randomNumber = createRandomNumber(state.min, state.max);
+    changeStatus(state.answer, randomNumber);
+  }
+
   function initializeGameSetting() {
+    //gameSetting초기화
     return { ...InitialGameConfig };
   }
 
-  function promptUserSetting() {
-    const promptCount = 1;
-    // changeState(state.status, gameStatus.USERSETTING[promptCount]);
+  function addCount(count) {
+    return count++;
+  }
+
+  function loopWhileCondition(loopFn, condition) {
+    //특정 조건 내에 함수 반복
+    while (condition) {
+      loopFn();
+    }
+  }
+
+  async function promptUserSetting() {
+    //count만큼 user setting받기
+    const result = await readLineAsync(returnSettingQuestion(state.status));
+
+    const isValid = checkPromptNumber(result);
+    if (!isValid) return console.log(returnSettingErrorMessage(state.status));
+
+    const nowStatus = state[gameStatus.USERSETTING_COUNT[promptCount]];
+    changeStatus(nowStatus, result);
+    addCount(promptCount);
   }
 }
 
-export async function playGame(state) {
+function readLineAsync(query) {
+  return new Promise((resolve, reject) => {
+    if (arguments.length !== 1) {
+      reject(new Error("arguments must be 1"));
+    }
 
-  while (state.status === gameStatus.PLAYING) {
-    promptUser();
-  }
+    if (typeof query !== "string") {
+      reject(new Error("query must be string"));
+    }
 
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  function checkStatus(state) {
-    //상태를 check함
-  }
-
+    rl.question(query, (input) => {
+      rl.close();
+      resolve(input);
+    });
+  });
 }
-export async function endGame() {}
+
+// export async function playGame(state) {
+//   const playState = { ...state };
+
+//   while (state.status === gameStatus.PLAYING) {
+//     promptUser();
+//   }
+
+//   async function promptUser() {
+//     //user의 응답을 받음
+//     return await readLineAsync();
+//   }
+// }
+// export async function endGame() {}
--- src/domain/index.js
+++ src/domain/index.js
@@ -1,23 +1,52 @@
-/**
- *
-domain의 하위의 모듈은 view하위의 모듈에 의존하지 말아야 한다는 의미는
-view에서 전달한 인수를 변경하면 안된다는 것으로 해석?
-함수내의 내부함수로 설정한 까닭은 
- */
-/**
-계산하고 결과를 출력하는 로직
-최소 최대 값 사이의 랜덤숫자 생성
-업 다운 정답 계산하여 출력
-이전 추측들을 보여준다
 
- */
+// export function gameData() {
+//   return {
+//     createRandomNumber: (min, max) => createRandomNumber(min, max),
+//     printUpDown,
+//     showInput,
+//     numberValidation,
+//     parseBoundary,
+//     isAnswerCorrect,
+//   };
+
+
+//   function printUpDown(answer, input) {
+//     if (answer > input) {
+//       return "업";
+//     }
+//     if (answer < input) {
+//       return "다운";
+//     }
+//   }
+
+//   function showInput(guessArr, guessInput) {
+//     const guess = guessArr;
+//     const toNumberInput = parseFloat(guessInput);
+//     if (Array.isArray(guess)) guess.push(toNumberInput);
+//     return guess;
+//   }
+
+//   function numberValidation(...userInput) {
+//     let isValid;
+//     isValid = userInput.every((input) => !isNaN(input));
+//     return isValid;
+//   }
+
+//   function parseBoundary(boundary) {
+//     const [first, second] = boundary.split(",");
+//     const minNumber = Math.min(first, second);
+//     const maxNumber = Math.max(first, second);
+//     return [minNumber, maxNumber];
+//   }
+
+//   function isAnswerCorrect(input, answer) {
+//     return input === answer;
+//   }
+// }
+
 export const gameStatus = {
   READY: "READY",
-  USERSETTING: {
-    ONE: "min",
-    TWO: "max",
-    THREE: "trialLimit",
-  },
+  USERSETTING_COUNT: ["MIN", "MAX", "TRIALLIMIT"],
   DEFAULTSETTING: "DEFAULTSETTING",
   PLAYING: "PLAYING",
   SUCCESS: "SUCCESS",
@@ -29,59 +58,53 @@ export const gameStatus = {
 export const InitialGameConfig = {
   status: gameStatus.READY,
   settingType: "user",
-  userTrial: 0,
+  userTrialCount: 0,
+  userTrials: [],
   trialLimit: 0,
-  answer: gameData().createRandomNumber(min, max),
+  answer: 0,
   min: 1,
   max: 50,
 };
 
-export function gameData() {
-  return {
-    createRandomNumber: (min, max) => createRandomNumber(min, max),
-    printUpDown,
-    showInput,
-    numberValidation,
-    parseBoundary,
-    isAnswerCorrect,
+export function returnSettingQuestion(state) {
+  //필요한 세팅 요청문구를 return
+  const dynamicWord = {
+    min: `최솟 값을`,
+    max: `최댓 값을`,
+    limit: `게임 시작을 위해 진행 가능 횟수를`,
   };
+  return `게임 시작을 위해 ${dynamicWord[state]} 입력해주세요.`;
+}
 
-  function createRandomNumber(min, max) {
-    const minNumber = min;
-    const maxNumber = max;
-    return Math.floor(Math.random() * maxNumber + minNumber);
-  }
+export function returnSettingErrorMessage(state) {
+  //세팅 에러 메시지를 return
+  const dynamicWord = {
+    min: "최솟 값",
+    max: "최댓 값",
+    trialLimit: "진행 가능 횟수",
+  };
+  return `${dynamicWord[state]}이 유효하지 않습니다.`;
+}
 
-  function printUpDown(answer, input) {
-    if (answer > input) {
-      return "업";
-    }
-    if (answer < input) {
-      return "다운";
-    }
-  }
+export const settingConfig = {
+  promptCount: 1,
+  totalSettingCount: 3,
+};
 
-  function showInput(guessArr, guessInput) {
-    const guess = guessArr;
-    const toNumberInput = parseFloat(guessInput);
-    if (Array.isArray(guess)) guess.push(toNumberInput);
-    return guess;
-  }
+export function createRandomNumber(min, max) {
+  const minNumber = min;
+  const maxNumber = max;
+  return Math.floor(Math.random() * maxNumber + minNumber);
+}
 
-  function numberValidation(...userInput) {
-    let isValid;
-    isValid = userInput.every((input) => !isNaN(input));
-    return isValid;
-  }
+export function changeStatus(state, change) {
+  //status를 변경
+  state = change;
+  return;
+}
 
-  function parseBoundary(boundary) {
-    const [first, second] = boundary.split(",");
-    const minNumber = Math.min(first, second);
-    const maxNumber = Math.max(first, second);
-    return [minNumber, maxNumber];
-  }
 
-  function isAnswerCorrect(input, answer) {
-    return input === answer;
-  }
+export function checkPromptNumber(prompt) {
+  //prompt가 숫자인지 check함
+  return userInput.every((input) => !isNaN(input));
 }
