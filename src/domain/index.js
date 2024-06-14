2476499039f948364b12df166b69271dfee95727
허제인 <codingjamee@gmail.com>
2024-06-14 21:55:08 +0900

    안내메시지함수를 리턴하는 함수 설정

--- src/domain/index.js
+++ src/domain/index.js
@@ -9,6 +9,7 @@ export const gameStatus = {
   FAIL: "FAIL",
   ASKRESTART: "ASKRESTART",
   END: "END",
+  NOTVALID_ANSWER: "NOTVALID_ANSWER",
 };
 
 export const InitialGameConfig = {
@@ -46,42 +47,6 @@ export const GameState = () => {
   };
 };
 
-export function returnSettingQuestion(state) {
-  //필요한 세팅 요청문구를 return
-  const dynamicWord = {
-    min: `최솟 값을`,
-    max: `최댓 값을`,
-    limit: `진행 가능 횟수를`,
-  };
-  return `게임 시작을 위해 ${dynamicWord[state]} 입력해주세요. :`;
-}
-
-export function returnSettingErrorMessage(state) {
-  //세팅 에러 메시지를 return
-  const dynamicWord = {
-    min: "최솟 값이",
-    max: "최댓 값이",
-    trialLimit: "진행 가능 횟수가",
-  };
-  return `${dynamicWord[state]} 유효하지 않습니다.`;
-}
-
-export function returnGameQuestion(state) {
-  return `컴퓨터가 ${state.min}~${state.max} 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.`;
-}
-
-export function returnGameErrorMessage() {
-  return "입력하신 숫자가 올바르지 않습니다.";
-}
-
-export function returnResultMessage(state) {
-  if (state === gameStatus.FAIL)
-    return `${state.trialLimit}회 초과! 숫자를 맞추지 못했습니다. (정답: ${state.answer})`;
-  if (state === gameStatus.SUCCESS)
-    return `정답! 
-  축하합니다! ${state.trialCount}번 만에 숫자를 맞추셨습니다.`;
-}
-
 export function createRandomNumber(min, max) {
   const minNumber = min;
   const maxNumber = max;
@@ -95,15 +60,6 @@ export function parseBoundary(boundary) {
   return [minNumber, maxNumber];
 }
 
-export function changeState(state, change) {
-  if (state.status) state.status = change;
-}
-
-export function changeAnswer(state, change) {
-  //status를 변경
-  state.answer = change;
-}
-
 export function checkPromptNumber(prompt) {
   //prompt가 숫자인지 check함
   return prompt.every((input) => !isNaN(input));
@@ -113,9 +69,52 @@ export function checkUpDown(answer, input) {
   return answer > input;
 }
 
-export async function asyncloopWhileCondition(loopFn, condition) {
+export async function runAsyncLoopWhileCondition(loopFn, condition) {
   //특정 조건 내에 함수 반복
   while (condition) {
     await loopFn();
   }
+}
+
+export const getGameInstructions = () => {
+  // 안내 메시지 함수들을 리턴
+
+  return {
+    returnSettingQuestion(state) {
+      //필요한 세팅 요청문구를 return
+      const dynamicWord = {
+        min: `최솟 값을`,
+        max: `최댓 값을`,
+        limit: `진행 가능 횟수를`,
+      };
+      return `게임 시작을 위해 ${dynamicWord[state]} 입력해주세요. :`;
+    },
+    returnSettingErrorMessage(state) {
+      //세팅 에러 메시지를 return
+      const dynamicWord = {
+        min: "최솟 값이",
+        max: "최댓 값이",
+        trialLimit: "진행 가능 횟수가",
+      };
+      return `${dynamicWord[state]} 유효하지 않습니다.`;
+    },
+    returnGameQuestion(state) {
+      return `컴퓨터가 ${state.min}~${state.max} 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.`;
+    },
+    returnGameErrorMessage() {
+      return "입력하신 숫자가 올바르지 않습니다.";
+    },
+    returnResultMessage(state) {
+      if (state === gameStatus.FAIL)
+        return `${state.trialLimit}회 초과! 숫자를 맞추지 못했습니다. (정답: ${state.answer})`;
+      if (state === gameStatus.SUCCESS)
+        return `정답! 
+    축하합니다! ${state.trialCount}번 만에 숫자를 맞추셨습니다.`;
+    },
+  };
+};
+
+export function showGuideMessage(message) {
+  // 메시지를 받아서 출력해주는 함수
+  console.log(message);
 }
