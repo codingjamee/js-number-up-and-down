376ecf90ec9216d4955d08005b287c0cc597c0a7
허제인 <codingjamee@gmail.com>
2024-06-19 23:47:26 +0900

    startGame computerSetting로직 위치 변경 및 playGame ui업데이트 로직 추가

--- src/UI/index.js
+++ src/UI/index.js
@@ -8,7 +8,9 @@ import {
   removeChildrenById,
   mutateDisabledBtn,
   addChildrenById,
+  getDetailIntsructionMessage,
 } from "../domain/index.js";
+import { getDivTemplate } from "../templates/index.js";
 import { addNumber, toNumber } from "../utils/util.js";
 
 export async function startGame() {
@@ -21,54 +23,53 @@ export async function startGame() {
   updateView({ state: gameState1.getState().status, btnText: "시작하기" });
 
   addSettingListener();
-  computerSetting();
 
   function addSettingListener() {
     addValidationListener();
     addSubmitListener();
 
-    //이 로직을 어디에 두어야 할지 생각해야함
-    //listener 작동 후 setting후에 설정 되어야함
-    // const targetStatus = gameState1.getState().PLAYING;
-    // gameState1.updateState("status", targetStatus);
-    // return playGame(gameState1);
-  }
+    function addValidationListener() {
+      settingIdArray.map((inputId) => {
+        addListenerById(inputId, "blur", inputIsNotValid);
+        addListenerById(inputId, "input", inputIsValid);
+      });
+      function inputIsNotValid(event) {
+        if (!isNumber(event.target.value)) {
+          addChildrenById(
+            inputId,
+            getGameInstructions().returnSettingErrorMessage(inputId)
+          );
+          mutateDisabledBtn("submitBtn", true);
+        }
+      }
 
-  function addValidationListener() {
-    settingIdArray.map((inputId) => {
-      addListenerById(inputId, "blur", inputIsNotValid);
-      addListenerById(inputId, "input", inputIsValid);
-    });
-    function inputIsNotValid(event) {
-      if (!isNumber(event.target.value)) {
-        addChildrenById(
-          inputId,
-          getGameInstructions().returnSettingErrorMessage(inputId)
-        );
-        mutateDisabledBtn("submitBtn", true);
+      function inputIsValid(event) {
+        if (isNumber(event.target.value)) {
+          removeChildrenById(inputId);
+          mutateDisabledBtn("submitBtn", false);
+        }
       }
     }
 
-    function inputIsValid(event) {
-      if (isNumber(event.target.value)) {
-        removeChildrenById(inputId);
-        mutateDisabledBtn("submitBtn", false);
+    function addSubmitListener() {
+      //submit할 때 input의 setting을 모두 설정
+      settingIdArray.map((inputId) => {
+        const input = document.getElementById(inputId);
+        gameState1.updateState(inputId, toNumber(input));
+      });
+      computerSetting();
+
+      function computerSetting() {
+        const { min, max } = gameState1.getState();
+        const randomNumber = createRandomNumber(min, max);
+
+        gameState1.updateState("answer", randomNumber);
+
+        const targetStatus = gameState1.getState().PLAYING;
+        gameState1.updateState("status", targetStatus);
+        return playGame(gameState1);
       }
     }
-  }
-
-  function addSubmitListener() {
-    //submit할 때 input의 setting을 모두 설정
-    settingIdArray.map((inputId) => {
-      const input = document.getElementById(inputId);
-      gameState1.updateState(inputId, toNumber(input));
-    });
-  }
-
-  function computerSetting() {
-    const { min, max } = gameState1.getState();
-    const randomNumber = createRandomNumber(min, max);
-    gameState1.updateState("answer", randomNumber);
   }
 }
 
@@ -82,6 +83,7 @@ export async function playGame(state) {
   });
 
   addPlayingListener();
+  //안내문구
 
   function addPlayingListener() {
     addValidationListener({ inputId: "userTrial" });
@@ -121,6 +123,15 @@ export async function playGame(state) {
         const userTry = document.getElementById("tryBtn");
         if (checkIsAnswer(userTry)) {
           playGameState1.updateState("status", gameStatus.SUCCESS);
+
+          const successMessage = getDetailIntsructionMessage(
+            getGameInstructions().getMessageTag("computer"),
+            getGameInstructions().returnResultMessage(
+              playGameState1.userTrialCount
+            )
+          );
+          addChildrenById("instructionView", successMessage);
+
           return endGame(playGameState1);
         }
 
@@ -138,23 +149,39 @@ export async function playGame(state) {
 
         function onValidGame(userInput) {
           const isUp = checkUpDown(playGameState1.answer, userInput);
-          if (isUp) return addChildrenById("up"); //문구 설정 필요
-          addChildrenById("down");
+          const trialCount = playGameState1.getState().userTrialCount;
 
-          addChildrenById(userInput); //usertry 추가
-          console.log(user.userTrials);
+          updateValidGameMessage();
 
-          playGameState1.updateState(
-            "userTrialCount",
-            addNumber(playGameState1.getState().userTrialCount)
-          );
+          playGameState1.updateState("userTrialCount", addNumber(trialCount));
+
+          function updateValidGameMessage() {
+            const userTryMessage = getDetailIntsructionMessage(
+              getMessageTag("user"),
+              userInput
+            );
+            const upAndDownMessage = getDetailIntsructionMessage(
+              getMessageTag("computer"),
+              getGameInstructions().returnNumberStatus(isUp)
+            );
+            const remainTrialMessage = getDetailIntsructionMessage(
+              getMessage("computer"),
+              getGameInstructions().returnRemainTrial(trialCount)
+            );
+
+            const userDiv = getDivTemplate(userTryMessage);
+            const upAndDownDiv = getDivTemplate(upAndDownMessage);
+            const remainDiv = getDivTemplate(remainTrialMessage);
+            const gameMessages = userDiv + upAndDownDiv + remainDiv;
+
+            addChildrenById("instructionView", gameMessages);
+          }
         }
-        
+
         function checkIsAnswer(userTry) {
           return userTry === playGameState1.answer;
         }
       }
-
     }
   }
 }
@@ -178,10 +205,6 @@ export async function endGame(playState, result) {
 
   function onSuccessGame() {
     changeState(endState.status, gameStatus.SUCCESS);
-  }
-
-  async function askRestart() {
-    return await alert("다시 시작하시겠습니까? (yes / no) : ");
   }
 }
 
